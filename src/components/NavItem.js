f0489b7ebe338de4de4f96499eda4cf95fd717c8
Jenish Girish Patel <jn336260@dal.ca>
2022-06-04 20:45:15 -0300

    commit-updated

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -8,28 +8,26 @@ import {
     MenuButton,
     MenuList
 } from '@chakra-ui/react'
-import NavHoverBox from '../components/NavHoverBox'
-
-export default function NavItem({ icon, title, description, active, navSize }) {
+export default function NavItem({ icon, title, description, active, navigation_size }) {
     return (
         <Flex
             mt={30}
             flexDir="column"
             w="100%"
-            alignItems={navSize === "small" ? "center" : "flex-start"}
+            alignItems={navigation_size === "small" ? "center" : "flex-start"}
         >
             <Menu placement="right">
                 <Link
-                    backgroundColor={active && "#AEC8CA"}
+                    backgroundColor={active && "yellow"}
                     p={3}
                     borderRadius={8}
-                    _hover={{ textDecor: 'none', backgroundColor: "#AEC8CA" }}
-                    w={navSize === "large" && "100%"}
+                    _hover={{ textDecor: 'none', backgroundColor: "green" }}
+                    w={navigation_size === "large" && "100%"}
                 >
                     <MenuButton w="100%">
                         <Flex>
                             <Icon as={icon} fontSize="xl" color={active ? "#82AAAD" : "gray.500"} />
-                            <Text ml={5} display={navSize === "small" ? "none" : "flex"}>{title}</Text>
+                            <Text ml={5} display={navigation_size === "small" ? "none" : "flex"}>{title}</Text>
                         </Flex>
                     </MenuButton>
                 </Link>
@@ -40,7 +38,6 @@ export default function NavItem({ icon, title, description, active, navSize }) {
                     h={200}
                     ml={5}
                 >
-                    <NavHoverBox title={title} icon={icon} description={description} />
                 </MenuList>
             </Menu>
         </Flex>
